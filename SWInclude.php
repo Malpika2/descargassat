295f09c6e71da0b670106493c83b3598f1ed2bf2
Anthony Santiago López <hamthony@hotmail.com>
2019-12-05 18:29:26 -0600

    Initial commit

---
+++ SWInclude.php
@@ -0,0 +1,6 @@
+<?php
+    file_exists("vendor/autoload.php")?require "vendor/autoload.php":"";
+    require dirname(__FILE__) . '/src/LoginXmlRequest.php';
+    require dirname(__FILE__) . '/src/RequestXmlRequest.php';
+    require dirname(__FILE__) . '/src/VerifyXmlRequest.php';  
+?>
